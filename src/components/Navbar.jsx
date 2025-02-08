3417448cdf1ddc8094fda74fb5d799432bb6bb74
pengsim <hghvb024@gmail.com>
2025-02-08 16:13:29 +0700

    a

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,14 @@ import { NavLink } from "react-router-dom/cjs/react-router-dom.min"
 export default function Navbar(){
     return(
         <>
-  <nav class="navbar navbar-expand-lg ">
+<nav class="navbar navbar-expand-lg">
   <div class="container-fluid">
-    <img src="image/bank.png" alt="" width={'50px'} />
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span> 
+  <img src="image/bank.png" alt="" width={'50px'} />
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
       <ul class="navbar-nav">
         <li class="nav-item">
         <NavLink to="/home" class="nav-link">
@@ -36,10 +37,11 @@ export default function Navbar(){
 
         <li class="nav-item">
         <NavLink to="/professor" class="nav-link">
-              My professor
+              professor
         </NavLink>
         </li>
       </ul>
+      </div>
     </div>
   </div>
 </nav>
