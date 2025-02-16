ef81314233c65e04d2c16c7baa135c554391199e
pengsim <hghvb024@gmail.com>
2025-02-16 15:32:09 +0700

    a

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ export default function Home(){
             <div className="container">
                 <h2 className="text-center">Welcome to my Research Project Team</h2>
                 <br />
-                <div className="p_text p-2">
+                <div className="p-2 p_text">
                     <h4>My project focuses on the number of students who use bank accounts across 30 universities, based on research from students at various universities such as <b>UC . Beltie . RUPP . RULE</b> ,Out of 30 students, 18 are female and 12 are male. For more details, you can download the PDF and Excel files.</h4>
                     <br />
                     <hr />
@@ -19,6 +19,11 @@ export default function Home(){
                         <li><a href="image/data.xlsx" download >Excel</a></li>
                     </ul>
                     </h4>
+                    
+                    <ul>
+                        <li><a href="image/data.xlsx" download >PDF</a></li>
+                    </ul>
+                
                     <hr />
                 </div>
                 <img src="" alt="" />
